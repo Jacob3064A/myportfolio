ea0176a9f8949e5cf7dde1a0d2a21f6d291ee254
Jacob3064A <thinkbox3064@gmail.com>
2023-03-23 10:46:43 +0900

    add projects.js data.js

---
+++ src/data.js
@@ -0,0 +1,20 @@
+// src/data.js
+
+const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description: "Lorem ipsum dolor sit amet",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React",
+    subtitle: "MERN",
+    description: "Lorem ipsum dolor sit amet",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export default projects;
